f3df6bf40605d68fc20f1c836f4618123a213433
đình trần <trandangdinh@dtu.edu.vn>
2024-11-22 21:36:20 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,25 @@ import logo from './assets/react.svg';
 
 
 const App =()=>{
+
+  const hoidanit ='phan';
+  const age =23
+  const data={
+    address :"Quang Nam",
+    country: "viet nam"
+  }
+
+
+
   return (
     <div className="todo-container">
        <div className="todo-tile"> Todo list </div>
        <TodoNew/>
-       <TodoData/>
+       <TodoData
+       name ={hoidanit}
+       age={age}
+       data={data}
+       />
 
       <div className='todo-image'>
         <img src={logo} className='logo'/>
--- src/components/todo/todoData.jsx
+++ src/components/todo/todoData.jsx
@@ -1,9 +1,19 @@
 
+// props là một object {}
+// {
+//   "name": "John",
+//   "age": 30
+// data :{}
+//}
+const {name,age,data}=props;
 
 
-const TodoData =()=>{
+const TodoData =(props)=>{
+  console.log(">>Check props",props);
+  
   return (
     <div className='todo-data'>
+      <div>My name is :{name} </div>
     <div>Learning react</div>
   </div>
   )
